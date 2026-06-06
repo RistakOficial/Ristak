8e93b810f08025c6ac0c579506c34c02b05aa705
Raúl Gómez <milemedia.mkt@gmail.com>
2026-06-06 17:19:26 -0600

    Replace mobile app icon

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ristak-branding-v14'
+const CACHE_NAME = 'ristak-branding-v15'
 const DEFAULT_NOTIFICATION_TITLE = 'Notificación nueva'
 const DEFAULT_NOTIFICATION_BODY = 'Tienes una notificación nueva.'
 const SHELL_ASSETS = [
